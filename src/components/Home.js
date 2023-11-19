494d5c1ad833e1eb77fde6178dddf501609325e8
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-11-19 12:24:38 +0300

    about component add div and child inside H2,P,imag

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
+  <div id="home"</div>
+  <div id="#about"</div>
   return <div>Home</div>;
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
+  <a href="#Home" </a>;
+  <a href="#About" </a>;
   return <nav>NavBar</nav>;
 }
 
 export default NavBar;
+
